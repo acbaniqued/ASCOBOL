0cfde5b7ee27e20c8e09cd6fb889e3a7b35b3e87
baniqued <baniquedmarck97@gmail.com>
2017-04-07 10:43:06 +0800

    Drop Down Enabled

--- WEBPROG/Yii2/config/web.php
+++ WEBPROG/Yii2/config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'enter your secret key here',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
